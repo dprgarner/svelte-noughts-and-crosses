85dd7009435e99bd776bc6bd917398a97850e552
David Garner <dprgarner@gmail.com>
2019-06-09 17:36:44 +0100

    Initial commit - with Prettier and sublime set-up

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import NewCpt from './NewCpt.svelte';
+
+  export let name;
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<div>
+  <h1>Hello {name.toUpperCase()}!</h1>
+
+  <NewCpt />
+</div>
